fedae20641fea0b568e6fd49738b77ade31f6fd4
rashmirathnayake <129378251+rashmirathnayake@users.noreply.github.com>
2023-05-04 15:02:14 +0530

    Create heapSort.cpp

---
+++ heapSort.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+  
+// function to heapify the tree
+void heapify(int arr[], int n, int i)
+{
+    int largest = i; 
+    int l = 2 * i + 1; 
+    int r = 2 * i + 2; 
+ 
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+ 
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+
+  
+// implementing heap sort
+void heapSort(int arr[], int n)
+{
+    // Build heap 
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+ 
+    // One by one extract an element from heap
+    for (int i = n - 1; i >= 0; i--) {
+        
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+  
+/* print contents of array */
+void displayArray(int arr[], int n)
+{
+   for (int i=0; i<n; ++i)
+   cout << arr[i] << " ";
+   cout << "\n";
+}
+  
+// main program
+int main()
+{
+   int heap_arr[] = {4,17,3,12,9,6};
+   int n = sizeof(heap_arr)/sizeof(heap_arr[0]);
+   cout<<"Input array"<<endl;
+   displayArray(heap_arr,n);
+  
+   heapSort(heap_arr, n);
+  
+   cout << "Sorted array"<<endl;
+   displayArray(heap_arr, n);
+}
